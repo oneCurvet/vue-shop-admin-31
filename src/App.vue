440d7c8d3735a9dacfac1b364bf77f20af773ccc
LL <983859194@qq.com>
2019-06-06 15:58:18 +0800

    修改了App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header></Header>
-    <el-button type="danger" round>危险按钮</el-button>
+    
     <router-view></router-view>
   </div>
 </template>
@@ -12,16 +12,7 @@ export default {
   components: {
     Header
   },
-  mounted() {
-    this.$axios({
-      method: "get",
-      url: "https://api.github.com/users"
-    }).then(res => {
-      console.log(res);
-    }).catch(err=>{
-      console.log(err);
-    })
-  }
+  
 };
 </script>
 
